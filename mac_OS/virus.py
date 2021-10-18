e1cc1316fbb464f1abb67ab23198e2570d28368a
jackrgilmore <81373974+jackrgilmore@users.noreply.github.com>
2021-10-18 12:06:04 -0400

    update

--- mac_OS/virus.py
+++ mac_OS/virus.py
@@ -1,10 +1,17 @@
+#!/usr/bin/env python3
+
+import os
 from os import remove
 from sys import argv
+from os.path import expanduser
+from os.path import join
 extension = str(".txt")
+name = ("spam")
 
 for i in range(100):
-    create = open("spam" + str(i + 1) + extension, "w+")
-    create.write("YOU'VE BEEN HACKED!")
-    create.close()
+    file = open(name + str(i+1) + extension, "w")
+    file.write("YOU'VE BEEN HACKED!")
+    file.close()
 
-remove(argv[0])
+#comment out deleting self
+#remove(argv[0])
