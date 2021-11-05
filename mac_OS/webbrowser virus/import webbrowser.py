e33681f4e9d95570de5532f602427e426bb9f7f6
jackrgilmore <81373974+jackrgilmore@users.noreply.github.com>
2021-11-05 14:36:58 -0400

    webbrowser/write companion virus

--- mac_OS/webbrowser virus/import webbrowser.py
+++ mac_OS/webbrowser virus/import webbrowser.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
-
+import os
 import webbrowser as wb
+name="hack"
+extension=".txt"
 
-for i in range (100):
-    wb.open("https://www.meta.com")
+for i in range (5):
+    wb.open("https://jackrgilmore.github.io/jackhack/")
+    file = open("Desktop/"+ name + str(i+1) + extension, "w")
+    file.write("YOU'VE BEEN HACKED!")
+    file.close()
+
+os.remove[os.argv(0)]
